21570778bf004b94f940a5260ad26dab95650168
Undef <gitlab@undef.tools>
2021-02-18 07:53:48 +0000

    Modify username in calamares config on installer images
    When the username is configured in the image calamares does not know
    which user to change the password of, causing the install to fail.
    
    This cannot be cleanly done live, as the username has to be discovered
    from uid and calamares has to modify its own configuration file.
    
    Solves https://gitlab.com/mobian1/calamares-settings-mobian/-/issues/2

--- scripts/setup-installer.sh
+++ scripts/setup-installer.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+USERNAME=$1
+[ "$USERNAME" ] || exit 1
+
 # Setup ondevice installer to start on boot
 systemctl enable calamaresfb.service
 
 # Disable eg25-manager (we don't need the modem during install)
 systemctl disable eg25-manager.service
+
+# Rename user so installer can change it's password
+if [ -f /etc/calamares/modules/mobile.conf ] && [ "$USERNAME" != "mobian" ]; then
+    sed -i "s/username: \"mobian\"/username: \"$USERNAME\"/" /etc/calamares/modules/mobile.conf
+fi
