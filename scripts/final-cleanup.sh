9a5f141ec450b2010b3079116896edd18a65109b
Arnaud Ferraris <arnaud.ferraris@gmail.com>
2021-05-18 01:23:25 +0200

    image: get rid of the final cleanup script
    This script is only used as a garbage collector for device-specific 
    intermediate files. As files under /tmp get automatically deleted, and 
    we can simply extend device recipes so they clean up their own mess, 
    this script can be dropped.

--- scripts/final-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Remove temporary dummy bootsector and u-boot binary
-rm -f /tmp/bootsector.bin \
-      /u-boot-sunxi-with-spl.bin \
-      /librem5-boot.img \
-      /tmp/kernel-dtb /tmp/bootimg
