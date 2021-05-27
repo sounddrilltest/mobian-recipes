7663b1636307405776a3211e4765a34d45a7fd09
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2021-05-27 16:14:23 +0200

    setup-repo: add security updates for bullseye

--- scripts/setup-repo.sh
+++ scripts/setup-repo.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 SUITE=$1
+NONFREE=$2
+
+if [ "$SUITE" = "bullseye" ]; then
+    COMPONENTS="main"
+    [ "$NONFREE" = "true" ] && COMPONENTS="$COMPONENTS non-free"
+    echo "deb http://security.debian.org/debian-security $SUITE-security $COMPONENTS" >> /etc/apt/sources.list
+fi
 
 # Set the proper suite in our sources.list
 sed -i "s/@@SUITE@@/${SUITE}/" /etc/apt/sources.list.d/mobian.list
