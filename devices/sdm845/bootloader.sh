3c7f0c8bd4612930c672c579768c2773d6ff0516
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2021-08-04 10:33:47 +0200

    sdm845: stop overwriting /boot partition
    We used a hack to generate Android boot images and store them in the 
    /boot partition. The proper way of doing so would rather be to store the 
    boot images on the rootfs, then use a script to move them into the 
    artifacts directory. By doing so, we can preserve /boot for future use.

--- devices/sdm845/bootloader.sh
+++ devices/sdm845/bootloader.sh
@@ -4,7 +4,6 @@ DEVICE="$1"
 OFFSET=0
 
 ROOTPART=$(grep f2fs /etc/fstab | awk '{ print $1; }')
-BOOTDEV=$(lsblk -o PATH,MOUNTPOINT -n | grep /boot | awk '{ print $1; }')
 KERNEL_VERSION=$(linux-version list)
 
 # Update the initramfs to make sure it's up-to-date
@@ -25,26 +24,16 @@ case "${DEVICE}" in
         ;;
 esac
 
-# HACK: Overwrite the /boot partition with the created bootimg so we can extract
-# it later (we don't need the contents of /boot anyway, the bootimg contains all
-# required data: kernel, DTB and initrd)
-cp /boot/vmlinuz-${KERNEL_VERSION} /boot/initrd.img-${KERNEL_VERSION} /tmp/
-umount /boot
-sed -i '/\/boot/d' /etc/fstab
-
 # Create a bootimg for each variant
 for variant in ${DTB_VARIANTS}; do
+    echo "Creating boot image for variant ${variant}"
+
     # Append DTB to kernel
-    cat /tmp/vmlinuz-${KERNEL_VERSION} /usr/lib/linux-image-${KERNEL_VERSION}/qcom/sdm845-${DTB_VENDOR}-${variant}.dtb > /tmp/kernel-dtb
+    cat /boot/vmlinuz-${KERNEL_VERSION} /usr/lib/linux-image-${KERNEL_VERSION}/qcom/sdm845-${DTB_VENDOR}-${variant}.dtb > /tmp/kernel-dtb
 
     # Create the bootimg as it's the only format recognized by the Android bootloader
-    mkbootimg --kernel /tmp/kernel-dtb --ramdisk /tmp/initrd.img-${KERNEL_VERSION} \
+    mkbootimg --kernel /tmp/kernel-dtb --ramdisk /boot/initrd.img-${KERNEL_VERSION} \
         --kernel_offset 0x8000 --ramdisk_offset 0x1000000 --tags_offset 0x100 \
         --pagesize 4096 --cmdline "mobian.root=${ROOTPART} mobian.variant=${variant} init=/sbin/init rw quiet splash" \
-        --base 0x0 --second_offset 0x0 -o /tmp/bootimg
-
-    dd if=/tmp/bootimg of=${BOOTDEV} bs=1M seek=${OFFSET}
-
-    # Put the next bootimg 64MB after the current one
-    OFFSET=$(expr ${OFFSET} + 64)
+        --base 0x0 --second_offset 0x0 -o /bootimg-${variant}
 done
