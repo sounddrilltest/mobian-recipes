6dbf379fe1c6d5ab219d0167f3e6a00ce0e52c1f
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2022-04-04 19:05:47 +0200

    sunxi64: bootloader: ensure DTBs are properly sync'ed
    Due to the order in which we install packages and create the image, DTBs
    are currently not automatically synced anymore. Fix that by restoring
    the corresponding bits in the bootloader script.

--- devices/sunxi/bootloader.sh
+++ devices/sunxi/bootloader.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-# Create boot menu file for u-boot
-u-boot-update
+# Sync DTBs to /boot so they're available to u-boot and update boot menu
+KERNEL_VERSION=$(linux-version list)
+for version in ${KERNEL_VERSION}; do
+    /etc/kernel/postinst.d/zz-u-boot-menu "${version}"
+done
