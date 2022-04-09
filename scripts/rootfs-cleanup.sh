7a1f5b69f0ea32bb261f3b1b9f2be886c1533e3c
Eugen Răhăian <eugen@rah.ro>
2022-04-09 14:07:45 +0300

    phosh: move disabling getty to -phosh yaml
    `rootfs-cleanup.sh` affects all environments. Disabling getty should
    happen only for Phosh, so moved that to `packages-phosh.yaml`.

--- scripts/rootfs-cleanup.sh
+++ scripts/rootfs-cleanup.sh
@@ -10,9 +10,6 @@ rm -f /etc/ssh/ssh_host_* \
       /etc/machine-id \
       /var/lib/dbus/machine-id
 
-# Disable getty on tty1, as we won't connect in console mode anyway
-systemctl disable getty@.service
-
 # FIXME: these are automatically installed on first boot, and block
 # the system startup for over 1 minute! Find out why this happens and
 # avoid this nasty hack
