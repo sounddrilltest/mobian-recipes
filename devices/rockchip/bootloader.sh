0e634947934b29a476804eba59e69197535a07e4
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2022-04-13 18:38:20 +0200

    bootloader: don't update bootloader config
    The bootloader is now installed at the same time as the kernel, on an
    already partitioned image. The initially generated config file is
    therefore valid and doesn't have to be regenerated.

--- devices/rockchip/bootloader.sh
+++ devices/rockchip/bootloader.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-set -e
-
-# Sync DTBs to /boot so they're available to u-boot and update boot menu
-KERNEL_VERSION=$(linux-version list)
-for version in ${KERNEL_VERSION}; do
-    /etc/kernel/postinst.d/zz-u-boot-menu "${version}"
-done
+exit 0
