fd289304eb712025238b3977e24f8ead5b9f1b28
Arnaud Ferraris <aferraris@debian.org>
2022-08-10 23:06:20 +0200

    librem5: bootloader: re-enable symlinks generation
    On the L5, we use a simple boot script relying on generic file names
    such as `Image.gz`, `initramfs.img` or the `dtb` folder. Those need to
    be explicitly generated on image creation due to possible race
    conditions during package installation (e.g. if the kernel is installed
    before the tweaks package, then symlinks won't be generated).

--- devices/librem5/bootloader.sh
+++ devices/librem5/bootloader.sh
@@ -2,3 +2,7 @@
 
 wget -O /librem5-boot.img \
 https://arm01.puri.sm/job/u-boot_builds/job/uboot_librem5_build/lastSuccessfulBuild/artifact/output/uboot-librem5/librem5-boot.img
+
+# Update generic symlinks to kernel/initramfs/dtbs
+KERNEL_VERSION=`linux-version list`
+/etc/kernel/postinst.d/zz-sync-dtb $KERNEL_VERSION
