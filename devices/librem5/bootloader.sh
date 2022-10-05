63d5d34c1a33f3b9fa813221c543065c80440e76
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2022-10-05 16:11:06 +0200

    librem5: bootloader: simplify image creation
    The current L5 images involves a few complex steps, especially for
    creating the bootloader protective partition. This originated from the
    belief that `u-boot` had to be flashed at offset 2KiB, requiring a GPT
    resizing to make it small enough to fit in the first 2K of the image
    (among other hacks).
    
    However this belief is actually wrong: Purism does provide an image
    (including a binary aimed at the M4 core) to be flashed at this offset,
    but the bootloader itself must be flashed at offset 0x8400 (33KiB), as
    specified in the i.MX8MQ TRM.
    
    We can therefore get rid of the complicated `gdisk`-based script and
    get back to using the good ol' `parted` for this task.

--- devices/librem5/bootloader.sh
+++ devices/librem5/bootloader.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-wget -O /librem5-boot.img \
-https://arm01.puri.sm/job/u-boot_builds/job/uboot_librem5_build/lastSuccessfulBuild/artifact/output/uboot-librem5/librem5-boot.img
+BOOTSTART="$1"
+
+wget -O /u-boot-librem5.imx \
+https://arm01.puri.sm/job/u-boot_builds/job/uboot_librem5_mainline_build/lastSuccessfulBuild/artifact/output/uboot-librem5/u-boot-librem5.imx
 
 # Update generic symlinks to kernel/initramfs/dtbs
 KERNEL_VERSION=`linux-version list`
@@ -9,40 +11,11 @@ KERNEL_VERSION=`linux-version list`
 
 TARGET_DISK=$(lsblk -n -o kname,pkname,mountpoint | grep ' /$' | awk '{ print $2 }')
 
-# We use gdisk for resizing the GPT and adding a "protective" partition for u-boot:
-# * x s 8: go into expert mode, set GPT size to 8 partitions max
-#          (4 sectors including the protective MBR)
-# * l 1: force alignment to 1 sector so we can do whatever we want (2048 by default)
-# * m n 8 8 \n b000: go back to normal mode and create partition 8 from sector 8 up
-#                    to the last available sector of type b000 (u-boot bootloader)
-# * x a 8 62: go into expert mode again and set flag 62 ("hidden") on partition 8
-#             (the one we created above)
-# *\n w Y: quit "set attribute" function, write partition table and confirm
-#
-# Notes:
-# 1. the GPT max size is 8 partitions on this device as u-boot MUST be flashed at
-#    a 2048kB offset, which is where sector n°4 starts.
-# 2. the new partition starts at sector 8 (and not sector 4) as it won't be
-#    considered by systemd-repart if it starts before 4096kB, and would therefore be
-#    overwritten, defeating its very purpose.
+# We use parted for adding a "protective" partition for u-boot:
+# * mkpart u-boot 66s ${BOOTSTART}: create "u-boot" partition from sector 66
+#                                   (33KiB) up to the start of the `/boot`
+#                                   partition
+# * toggle 3 hidden: set flag "hidden" on partition 3 (the one we just created)
 
-/usr/sbin/gdisk /dev/$TARGET_DISK <<EOF
-x
-s
-8
-l
-1
-m
-n
-8
-8
-
-b000
-x
-a
-8
-62
-
-w
-Y
-EOF
+/usr/sbin/parted /dev/$TARGET_DISK -s mkpart u-boot 66s ${BOOTSTART}
+/usr/sbin/parted /dev/$TARGET_DISK -s toggle 3 hidden
