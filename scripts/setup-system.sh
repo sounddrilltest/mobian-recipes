8cc3865f9dcee0e5c574235d6b82c73d072d1dfb
Arnaud Ferraris <aferraris@debian.org>
2022-11-03 18:32:29 +0100

    include: packages-phosh: install phog by default
    This requires **not** enabling phosh's systemd service, as `phog` will
    be auto-enabled and we want to avoid that kind of conflicts.

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -6,11 +6,6 @@ echo $1 > /etc/hostname
 # Change plymouth default theme
 plymouth-set-default-theme mobian
 
-# Load phosh on startup if package is installed
-if [ -f /usr/bin/phosh ]; then
-    systemctl enable phosh.service
-fi
-
 # systemd-firstboot requires user input, which isn't possible
 # on mobile devices
 systemctl disable systemd-firstboot.service
