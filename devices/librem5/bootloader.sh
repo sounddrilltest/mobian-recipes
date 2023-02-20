808531847c0071300a4ceb0cb8215f1b9e3f27f4
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2023-02-20 10:36:29 +0100

    librem5: bootloader: flash u-boot from the "distroboot" MR
    Current Purism u-boot is failing when it finds an `extlinux.conf` file,
    as it lacks some plumbing for dealing with compressed kernels. Let's use
    the CI artifact from
    https://source.puri.sm/Librem5/uboot-imx/-/merge_requests/55 instead, as
    it will work better for our use-case.
    
    That way we can also avoid calling the `zz-sync-dtb` script as
    generating a valid `extlinux.conf` will be enough now.

--- devices/librem5/bootloader.sh
+++ devices/librem5/bootloader.sh
@@ -3,13 +3,10 @@
 BOOTSTART="$1"
 
 wget -O /u-boot-librem5.imx \
-https://arm01.puri.sm/job/u-boot_builds/job/uboot_librem5_mainline_build/lastSuccessfulBuild/artifact/output/uboot-librem5/u-boot-librem5.imx
-
-# Update generic symlinks to kernel/initramfs/dtbs
-KERNEL_VERSION=$(linux-version list)
-/etc/kernel/postinst.d/zz-sync-dtb "${KERNEL_VERSION}"
+https://source.puri.sm/a-wai/uboot-imx/-/jobs/405334/artifacts/raw/debian/output/u-boot.imx
 
 # Re-generate extlinux.conf to ensure we have a valid one
+KERNEL_VERSION=$(linux-version list)
 /etc/kernel/postinst.d/zz-u-boot-menu "${KERNEL_VERSION}"
 
 TARGET_DISK=$(lsblk -n -o kname,pkname,mountpoint | grep ' /$' | awk '{ print $2 }')
