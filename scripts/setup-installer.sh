a3efed457b5185c67f418ea286316bf672dbfdd0
undef <gitlab@undef.tools>
2023-05-19 08:28:03 +0000

    scripts/setup-installer: Calamares is enabled by the package
    The Calamares-settings-mobian package replaced the calamaresfb service
    with calamares-settings-mobian.service a while ago. Drop it from the
    installer setup.

--- scripts/setup-installer.sh
+++ scripts/setup-installer.sh
@@ -10,9 +10,6 @@ cat > /etc/systemd/logind.conf.d/10-ignore-power-key.conf << EOF
 HandlePowerKey=ignore
 EOF
 
-# Setup ondevice installer to start on boot
-systemctl enable calamaresfb.service
-
 # Disable eg25-manager (we don't need the modem during install)
 systemctl disable eg25-manager.service
 
